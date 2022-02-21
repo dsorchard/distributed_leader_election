e359283877a1c40a8ddbe3528c418957c52843d5
arjunsk <admin@arjunsk.com>
2022-02-20 19:06:24 -0600

    Printing Output

--- utd-dc-project1/src/main/java/edu/utd/dc/project0/core/io/sharedmemory/SharedMemoryBus.java
+++ utd-dc-project1/src/main/java/edu/utd/dc/project0/core/io/sharedmemory/SharedMemoryBus.java
@@ -4,20 +4,20 @@ import edu.utd.dc.project0.core.io.sharedmemory.domain.Message;
 import edu.utd.dc.project0.core.support.ProcessId;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class SharedMemoryBus {
 
-  public static Map<ProcessId, List<Destination>> list = new LinkedHashMap<>();
+  public static Map<ProcessId, List<Destination>> list = new ConcurrentHashMap<>();
 
   public static void register(ProcessId source, ProcessId destination, Listener listener) {
     list.putIfAbsent(source, new ArrayList<>());
     list.get(source).add(new Destination(destination, listener));
   }
 
-  public static void send(ProcessId destinationId, Message message) {
+  public static synchronized void send(ProcessId destinationId, Message message) {
     ProcessId sourceId = message._source;
 
     for (Destination destination : list.get(sourceId))
